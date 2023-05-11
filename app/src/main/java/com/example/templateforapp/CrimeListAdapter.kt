8079a33fdaa40f3eed0e2b1d8a7d7abe793d6245
rus <rustamalimidjanov@gmail.com>
2023-05-11 21:27:05 +0300

    add coroutines

--- app/src/main/java/com/example/templateforapp/CrimeListAdapter.kt
+++ app/src/main/java/com/example/templateforapp/CrimeListAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.templateforapp
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
@@ -18,6 +19,11 @@ class CrimeHolder(
                 "${crime.title} clicked",
                 Toast.LENGTH_SHORT,
             ).show()
+        }
+        binding.crimeSolved.visibility = if(crime.isSolved){
+            View.VISIBLE
+        } else {
+            View.GONE
         }
     }
 
